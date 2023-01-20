8088a858fa9c67a09b605c80e08c4a8140e6e96d
sbentaf <53099943+sbentaf@users.noreply.github.com>
2023-01-20 02:44:58 +0100

    first commit

---
+++ ft_printf.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbenouat <sbenouat@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/20 02:41:04 by sbenouat          #+#    #+#             */
+/*   Updated: 2023/01/20 02:42:59 by sbenouat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_printf(char const *s, ...);
+int	ft_putchar(int c);
+int	ft_putstr(char *str);
+int	ft_putnbr(int n);
+int	ft_putnbr_unsigned(unsigned int n);
+int	ft_hexa(char c, int n);
+
+#endif
